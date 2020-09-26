b11f0963f8d9e4ed6c5575c8000d3bb189b49802
shivam saini <59383963+shivam007-ss@users.noreply.github.com>
2020-09-26 23:02:01 +0530

    add some usefull features to our website

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask ,url_for,render_template,redirect,url_for
+from flask import Flask ,url_for,render_template,redirect,url_for,request
 
 app=Flask(__name__)
+
+sucribe=[]
 
 @app.route("/")
 def main():
@@ -11,6 +13,16 @@ def about():
     names=['Shivam','Shubham','rohit']
     return render_template("about.html",names=names)
 
+@app.route("/form",methods=["POST"])
+def form():
+    first=request.form.get('firstname')
+    email=request.form.get('email')
+    sucribe.append((first,email))
+    return render_template("form.html",sucribe=sucribe)
+
+@app.route("/suscribe")
+def suscribe():
+    return render_template('suscribe.html')
 
 if __name__=="__main__":
     app.run(debug=True)
