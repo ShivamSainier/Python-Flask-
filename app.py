7f9f071a61faa6484a5914b21c7951b2d07b8d5e
shivam saini <59383963+shivam007-ss@users.noreply.github.com>
2020-09-30 19:08:53 +0530

    add a flash in our web page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,redirect,request,url_for,session
+from flask import Flask,render_template,redirect,request,url_for,session,flash
 
 
 app=Flask(__name__)
@@ -13,6 +13,7 @@ def login():
     if request.method=="POST":
         name=request.form.get("name")
         session['user']=name
+        
         return redirect(url_for("user"))
     else:
         return render_template("login.html")
@@ -21,12 +22,14 @@ def login():
 @app.route("/user")
 def user():
     if 'user' in session:
+        flash("you just log in Thank you ")
         user=session['user']
         return render_template("main.html",user=user)
     else:
         return redirect(url_for('login'))
 @app.route("/logout")
 def logout():
+    flash("You just log out !")
     session.pop("user",None)
     return redirect(url_for("main"))
 
