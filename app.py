902e1d513cb09c33abee3559c6e763333eb5799d
shivam saini <59383963+shivam007-ss@users.noreply.github.com>
2020-10-01 21:46:12 +0530

    Add a flash message

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template,redirect,request,url_for,session,flash
-from form import signform
+from form import resister_form,login_form
 
 
 
@@ -10,17 +10,6 @@ def main():
     return render_template("main.html")
 
 
-@app.route("/login",methods=["POST","GET"])
-def login():
-    if request.method=="POST":
-        name=request.form.get("name")
-        session['user']=name
-        
-        return redirect(url_for("user"))
-    else:
-        return render_template("login.html")
-
-
 @app.route("/user")
 def user():
     if 'user' in session:
@@ -29,15 +18,25 @@ def user():
         return render_template("main.html",user=user)
     else:
         return redirect(url_for('login'))
-@app.route("/logout")
-def logout():
-    flash("You just log out !")
-    session.pop("user",None)
-    return redirect(url_for("main"))
 
-@app.route("/sign")
-def sign():
-    form=signform()
-    return render_template('sign .html',form=form)
+
+@app.route("/resister",methods=["GET","POST"])
+def resister():
+    sign_up=resister_form()
+    if sign_up.validate_on_submit():
+        flash("You just Resistered!!")
+        return redirect(url_for('main'))
+    return render_template('resister.html',sign_up=sign_up)
+
+@app.route('/login',methods=["GET","POST"])
+def login():
+    l=login_form()
+    if l.validate_on_submit():
+        flash("You just logged in !!")
+        return redirect(url_for('main'))
+    else:
+        return render_template("login.html",l=l)
+
+
 if __name__=="__main__":
     app.run(debug=True)
