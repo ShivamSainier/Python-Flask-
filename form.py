634d9e5caa0f5fbbff58225f8512b944e1da51a9
shivam saini <59383963+shivam007-ss@users.noreply.github.com>
2020-10-01 21:46:41 +0530

    Create Resistration and login valdation forms

--- form.py
+++ form.py
@@ -1,7 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField
+from wtforms import StringField , PasswordField,SubmitField
+from wtforms.validators import DataRequired,Email,Length,EqualTo
 
-class signform(FlaskForm):
-    username=StringField('Username')
-    password=PasswordField("password")
+class resister_form(FlaskForm):
+    username=StringField("username",validators=[DataRequired(),Length(min=3,max=6)])
+    pasword=StringField("password",validators=[DataRequired()])
+    email=StringField("email",validators=[DataRequired(),Email()])
+    confirm_password=StringField("confirm_password",validators=[DataRequired(),EqualTo('pasword')])
     submit=SubmitField('sign up')
+
+
+class login_form(FlaskForm):
+    username=StringField("username",validators=[DataRequired(),Length(min=3,max=6)])
+    password=StringField("password",validators=[DataRequired()])
+    submit=SubmitField("log in")
