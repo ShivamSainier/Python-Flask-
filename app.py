6f26835e3f918e97639130f0e333a5f278f44bc8
shivam saini <59383963+shivam007-ss@users.noreply.github.com>
2020-10-02 13:05:13 +0530

    Add database column

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
 from flask import Flask,render_template,redirect,request,url_for,session,flash
 from form import resister_form,login_form
-
-
+from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
 app.secret_key="hello"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/users'
+db=SQLAlchemy(app)
+
+class user(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    Username=db.Column(db.String(20),nullable=False)
+    Email=db.Column(db.String(120),nullable=False)
+    Password=db.Column(db.String(20),nullable=False)
+    Image=db.Column(db.String(40),nullable=False)
+    posts=db.relationship('posts',backref='author',lazy=True)
+
+class posts(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    Title=db.Column(db.Text,nullable=False)
+    Content=db.Column(db.Text,nullable=False)
+    Date_posted=db.Column(db.DateTime,nullable=False)
+    user_id=db.Column(db.Integer,db.ForeignKey('users.id'),nullable=False)
+
+
 @app.route("/")
 def main():
     return render_template("main.html")
@@ -24,8 +42,9 @@ def user():
 def resister():
     sign_up=resister_form()
     if sign_up.validate_on_submit():
+    
         flash("You just Resistered!!")
-        return redirect(url_for('main'))
+        return redirect('/')
     return render_template('resister.html',sign_up=sign_up)
 
 @app.route('/login',methods=["GET","POST"])
@@ -33,7 +52,7 @@ def login():
     l=login_form()
     if l.validate_on_submit():
         flash("You just logged in !!")
-        return redirect(url_for('main'))
+        return redirect("/")
     else:
         return render_template("login.html",l=l)
 
