835af4901f16dc313e45302759797adc0ba5bb65
shivam saini <59383963+shivam007-ss@users.noreply.github.com>
2020-10-03 20:22:21 +0530

    Add login system into webpage

--- flaskblog/form.py
+++ flaskblog/form.py
@@ -1,19 +1,22 @@
 from wtforms import StringField , PasswordField,SubmitField
 from wtforms.validators import DataRequired,Email,Length,EqualTo,ValidationError
 from flask_wtf import FlaskForm
-
+from flaskblog.models import user,posts
 
 
 class resister_form(FlaskForm):
     username=StringField("username",validators=[DataRequired(),Length(min=3,max=6)])
-    pasword=PasswordField("password",validators=[DataRequired()])
+    password=PasswordField("password",validators=[DataRequired()])
     email=StringField("email",validators=[DataRequired(),Email()])
-    confirm_password=PasswordField("confirm_password",validators=[DataRequired(),EqualTo('pasword')])
+    confirm_password=PasswordField("confirm_password",validators=[DataRequired(),EqualTo('password')])
     submit=SubmitField('submit')
-
     def validation_username(self,username):
-        use=user.query.filter_by(Username=username.data).first()
-        if use:
+        userr=user.query.filter_by(username=username.data).first()
+        if userr:
+            raise ValidationError("it is already exist try something new")
+    def validation_email(self,email):
+        userr=user.query.filter_by(email=email.data).first()
+        if userr:
             raise ValidationError("it is already exist try something new")
 
 
--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,15 +1,30 @@
-from flaskblog import db
+from flaskblog import db,login_manager
+from datetime import datetime
+from flask_login import UserMixin
 
 
-class user(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return user.query.get(int(user_id))
+
+
+class user(db.Model,UserMixin):
     id=db.Column(db.Integer,primary_key=True)
-    Username=db.Column(db.String(20),nullable=False,unique=True)
-    Email=db.Column(db.String(120),nullable=False,unique=True)
-    Password=db.Column(db.String(20),nullable=False,unique=True)
+    username=db.Column(db.String(20),nullable=False,unique=True)
+    email=db.Column(db.String(120),nullable=False,unique=True)
+    image_file=db.Column(db.String(20),nullable=False,default='default.jpg')
+    password=db.Column(db.String(20),nullable=False)
+    post=db.relationship('posts',backref='author',lazy=True)
+
+    def __repr__(self):
+        return f"user('{self.username}','{self.email}','{self.image_file}')"
 
 
 class posts(db.Model):
     id=db.Column(db.Integer,primary_key=True)
     title=db.Column(db.Text,nullable=False)
     content=db.Column(db.Text,nullable=False)
-    date_posted=db.Column(db.DateTime,nullable=False)
+    date_posted=db.Column(db.DateTime,nullable=False,default=datetime.utcnow)
+    user_id=db.Column(db.Integer,db.ForeignKey('user.id'),nullable=True)
+    def __repr__(self):
+        return f"user('{self.title}','{self.date_posted}')"
