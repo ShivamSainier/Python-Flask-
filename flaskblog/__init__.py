6e36bfac7c32d48f6ca954af54643a4d0c550bbc
shivam saini <59383963+shivam007-ss@users.noreply.github.com>
2020-10-03 20:21:55 +0530

    import loginmanager

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_login import LoginManager
 
 
 app=Flask(__name__)
 app.secret_key="hello"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/users'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db=SQLAlchemy(app)
-
+login_manager=LoginManager(app)
 
 from flaskblog import routes
